318764d522d3d065301fb6985e6ef3c2dc698a0f
Mario Cuesta Rivavelarde <cuestam@ui.sci.unican.es>
2026-03-26 10:43:16 +0100

    adapt model to paper for mnist

--- src/model.py
+++ src/model.py
@@ -84,17 +84,16 @@ class CNN_MNIST(nn.Module):
         # 2x2 max pooling
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         
-        self.fc1 = nn.Linear(16 * 10 * 10, 120)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, num_classes)
 
     def forward(self, x):
 
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
         
-        x = torch.flatten(x, 1) 
+        x = x.view(-1, 16 * 4 * 4)
         
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
